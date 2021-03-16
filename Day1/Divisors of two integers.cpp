3ef24b176bf640b98f1a805c8c03108be5a7acf9
DELL <rohitmanathiya1tp@gmail.com>
2021-03-16 11:04:42 +0530

    codeforces code link is https://codeforces.com/problemset/problem/1108/B

---
+++ Day1/Divisors of two integers.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n; cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	sort(a,a+n);
+	int x=a[n-1];
+	for(int i=1;i<n;i++){
+		if(a[i]!=0 && x%a[i]==0){
+			a[i]=0;
+		}
+	}
+	int y=0;
+	for(int i=0;i<n;i++){
+		y=max(y,a[i]);
+	}
+	cout<<x<<" "<<y;
+	return 0;
+	
+}
